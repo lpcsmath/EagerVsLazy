f467ef1a8173a285941205fc1091caaa9b1499b3
Lothar Pfeiler <Lothar.Pfeiler@csmath.de>
2019-04-27 15:36:35 +0200

    adjusted example 2

--- Example2/CallByName.js
+++ Example2/CallByName.js
@@ -3,10 +3,11 @@ function fib (n) {
     return fib(n - 1) + fib(n - 2);
 }
 
-
 function f (a, b) {
     return a() || b() + b();
 }
-var lazyA = function () { return 0; }
-var lazyB = function () { return fib(40); }
+
+var lazyA = function () { return 0; };
+var lazyB = function () { return fib(40); };
+
 console.log(f(lazyA, lazyB));
