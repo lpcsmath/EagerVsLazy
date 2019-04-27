8d857deccd4fc68e26bfffbcde8d9471eb45e047
Lothar Pfeiler <Lothar.Pfeiler@csmath.de>
2019-04-27 11:09:32 +0200

    added callByName for avg calculation

--- Example3/CallByName.js
+++ Example3/CallByName.js
@@ -17,6 +17,23 @@ function f (a, b, c, d, e, g, h, i, j, k, l, m, n, o, p, q, r, s, t) {
 
 var lazyA = function () { return 0; }
 var lazyB = function () { return fib(40); }
-console.log(f(lazyA, lazyB, lazyB, lazyB, lazyB, lazyB, lazyB,
-                     lazyB, lazyB, lazyB, lazyB, lazyB, lazyB,
-                     lazyB, lazyB, lazyB, lazyB, lazyB, lazyB));
+var lazyC = function () { return fib(40); }
+var lazyD = function () { return fib(40); }
+var lazyE = function () { return fib(40); }
+var lazyG = function () { return fib(40); }
+var lazyH = function () { return fib(40); }
+var lazyI = function () { return fib(40); }
+var lazyJ = function () { return fib(40); }
+var lazyK = function () { return fib(40); }
+var lazyL = function () { return fib(40); }
+var lazyM = function () { return fib(40); }
+var lazyN = function () { return fib(40); }
+var lazyO = function () { return fib(40); }
+var lazyP = function () { return fib(40); }
+var lazyQ = function () { return fib(40); }
+var lazyR = function () { return fib(40); }
+var lazyS = function () { return fib(40); }
+var lazyT = function () { return fib(40); }
+console.log(f(lazyA, lazyB, lazyC, lazyD, lazyE, lazyG, lazyH,
+                     lazyI, lazyJ, lazyK, lazyL, lazyM, lazyN,
+                     lazyO, lazyP, lazyQ, lazyR, lazyS, lazyT));
